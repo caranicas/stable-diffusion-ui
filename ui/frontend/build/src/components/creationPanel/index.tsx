c28e84db7e9b62ec8282c55c0e73e10f158102cf
caranicas <kendrick.caranicas@gmail.com>
2022-09-13 12:38:26 -0400

    working toward image selection

--- ui/frontend/build/src/components/creationPanel/index.tsx
+++ ui/frontend/build/src/components/creationPanel/index.tsx
@@ -20,6 +20,23 @@ export default function CreationPanel() {
     setRequestOption("prompt", event.target.value);
   };
 
+  const _handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log("file select", event);
+    const file = event.target.files[0];
+
+    console.log("file", file);
+
+    // if (file) {
+    //   const reader = new FileReader();
+    //   reader.onload = (e) => {
+    //     if (e.target) {
+    //       setRequestOption("file", e.target.result);
+    //     }
+    //   };
+    //   reader.readAsDataURL(file);
+    // }
+  };
+
   return (
     <div className="create-panel">
       <div className="basic-create">
@@ -28,9 +45,19 @@ export default function CreationPanel() {
           <textarea value={promptText} onChange={handlePromptChange}></textarea>
         </div>
 
-        <div className="seed-image">
+        {/* <div className="seed-image">
           <p>Seed Image</p>
           <input type="file" accept="image/*" />
+        </div> */}
+
+
+        <div id="editor-inputs-init-image" class="row">
+          <label for="init_image"><b>Initial Image:</b> (optional) </label> 
+            <input id="init_image" name="init_image" type="file"  onChange={_handleFileSelect}/><br/>
+          <div id="init_image_preview_container" class="image_preview_container">
+            <img id="init_image_preview" src="" width="100" height="100" />
+            <button id="init_image_clear" class="image_clear_btn">X</button>
+          </div>
         </div>
 
         <MakeButton></MakeButton>
